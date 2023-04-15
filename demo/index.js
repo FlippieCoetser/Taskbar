24f269dcbf8f795aa27d83f25e26b17069eadb5d
Flippie.Coetser <flippie.coetser@gmail.com>
2023-04-15 11:49:26 -0700

    CODE RUB: Reorder Imports

--- demo/index.js
+++ demo/index.js
@@ -1,23 +1,20 @@
-
-import { Pin      } from '@browser-modules/button.pin'
-import { Restore  } from '@browser-modules/button.restore'
-import { Close    } from '@browser-modules/button.close'
-import { Minimize } from '@browser-modules/button.minimize'
-
 import { TemplateLoader } from '../lib/utilities/templateLoader.js'
-
 await TemplateLoader.load("../lib/templates/Button.Pin.template.html")
 await TemplateLoader.load("../lib/templates/Button.Restore.template.html")
 await TemplateLoader.load("../lib/templates/Button.Close.template.html")
 await TemplateLoader.load("../lib/templates/Button.Minimize.template.html")
 console.log('templates: Loaded')
 
+import { Pin      } from '@browser-modules/button.pin'
+import { Restore  } from '@browser-modules/button.restore'
+import { Close    } from '@browser-modules/button.close'
+import { Minimize } from '@browser-modules/button.minimize'
+
 customElements.define(Pin.tag, Pin)
 customElements.define(Restore.tag, Restore)
 customElements.define(Close.tag, Close)
 customElements.define(Minimize.tag, Minimize)
 
-// Load Imperative Interface
 window.pin = document.querySelector('button-pin')
 window.restore = document.querySelector('button-restore')
 window.close = document.querySelector('button-close')
