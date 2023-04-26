e85eacfab1c74b7390f9cd98e95af3d23826f4d4
Flippie.Coetser <flippie.coetser@gmail.com>
2023-04-26 09:51:06 -0700

    CONFIG: Add Index.js for NPM Package

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @module Taskbar
+ */
+
+export { Taskbar } from "./Taskbar.js";
+export { Configuration } from "./Taskbar.config.js";
+export { Attribute, Attributes } from "./Taskbar.metadata.js";
+export { Visible, Visibility } from "./Taskbar.metadata.js";
+export { State, States } from "./Taskbar.metadata.js";
+export { Orientation, Orientations } from "./Taskbar.metadata.js";
+export { Window, Windows } from "./Taskbar.metadata.js";
+export { Event, Events } from "./Taskbar.metadata.js";
+export { Handler } from "./Taskbar.metadata.js";
